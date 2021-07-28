96ee084780f4e88967e36314c86c6016619df9aa
HelloImCDD <cddgamer@gmail.com>
2021-07-29 01:49:32 +0200

    Added put and delete functions to base adapter

--- frontent/src/adapters/BaseAdapter.jsx
+++ frontent/src/adapters/BaseAdapter.jsx
@@ -17,3 +17,13 @@ export function post(url, requestData) {
 	const axios = getAxiosInstance();
 	return axios.post(url, requestData);
 }
+
+export function put(url, putData) {
+	const axios = getAxiosInstance();
+	return axios.put(url, putData);
+}
+
+export function del(url, deleteData) {
+	const axios = getAxiosInstance();
+	return axios.delete(url,deleteData);
+}
